479d9c78e8b85266ecf31c8999388d444482eb66
Anik-36 <delighteraranik@gmail.com>
2025-11-14 00:27:21 +0600

    something changes

--- 03-Javascript-Fundamental-part-2/11-coding-challenge/script.js
+++ 03-Javascript-Fundamental-part-2/11-coding-challenge/script.js
@@ -8,15 +8,13 @@ const jonasArray = [
   ["Michel", "Jonas", "Levinho", "Seveu"],
 ];
 
-
-
 // craating a Object
 // object ekta unstructure data structure. vitorer element gulare bole properties, r prottekta k bole keyword, indexing nai kono.
 
 const jonas = {
-    firstname : 'Jonas',
-    lastname : 'Schebsmann',
-    age : 2037 - 1991;
-    job : 'teacher';
-    friends : ["Michel", "Jonas", "Levinho", "Seveu"];
-}
+  firstname: "Jonas",
+  lastname: "Schebsmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michel", "Jonas", "Levinho", "Seveu"],
+};
