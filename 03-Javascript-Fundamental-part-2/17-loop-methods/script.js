0ad4a57b1d0091849a71944b335b799711765dae
Anik-36 <delighteraranik@gmail.com>
2025-11-17 15:44:50 +0600

    learn about printing array by loops

---
+++ 03-Javascript-Fundamental-part-2/17-loop-methods/script.js
@@ -0,0 +1,12 @@
+const jonas = [
+  "jonas",
+  "schabsmann",
+  2037 - 1991,
+  "teacher",
+  ["levinho", "seveu", "michel"],
+  true,
+];
+
+for (let i = 0; i < jonas.length; i++) {
+  console.log(jonas[i], typeof jonas[i]);
+}
