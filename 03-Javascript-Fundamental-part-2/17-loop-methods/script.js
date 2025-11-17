1968e7c570c00509d0f79683e12142657be16952
Anik-36 <delighteraranik@gmail.com>
2025-11-17 18:42:50 +0600

    learn about continue and break

--- 03-Javascript-Fundamental-part-2/17-loop-methods/script.js
+++ 03-Javascript-Fundamental-part-2/17-loop-methods/script.js
@@ -7,6 +7,35 @@ const jonas = [
   true,
 ];
 
+const types = [];
 for (let i = 0; i < jonas.length; i++) {
+  // read the array
+  console.log(jonas[i], typeof jonas[i]);
+
+  //filling types of array
+  // types[i] = typeof jonas[i];
+  types.push(jonas[i]);
+}
+
+console.log(types);
+
+const years = [1991, 2020, 2013, 2001];
+
+const ages = [];
+for (let i = 0; i < years.length; i++) {
+  ages.push(2037 - years[i]);
+}
+
+console.log(ages);
+
+console.log("----- ONLY STRING PRINTED ------");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] != "string") continue;
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log("----- ONLY BERAK WHEN ACCESS NUMBER ------");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] == "number") break;
   console.log(jonas[i], typeof jonas[i]);
 }
