7e3b82127ff391cab70613373a559d798f17bc0c
Anik-36 <delighteraranik@gmail.com>
2025-11-18 19:13:03 +0600

    determine the amplitude

--- 04-Developer-skills-and-editor-setup/01-calculate-average-amplitude/script.js
+++ 04-Developer-skills-and-editor-setup/01-calculate-average-amplitude/script.js
@@ -1,13 +1,18 @@
+const temparature = [-1, 3, -5, -6, 'error', 4, 18, 17, 1, 23, -5, 34];
+
 const calcAverageAmplitude = function (temps) {
   let max = temps[0];
   let min = temps[0];
   for (let i = 0; i < temps.length; i++) {
     let currTemp = temps[i];
+    if (typeof currTemp !== 'number') continue;
 
     if (currTemp > max) max = currTemp;
     if (currTemp < min) min = currTemp;
   }
-  console.log(min, max);
+  console.log(max, min);
+  return max - min;
 };
 
-calcAverageAmplitude([3, 5, 1, 0, -34, 16]);
+const amplitude = calcAverageAmplitude(temparature);
+console.log(amplitude);
