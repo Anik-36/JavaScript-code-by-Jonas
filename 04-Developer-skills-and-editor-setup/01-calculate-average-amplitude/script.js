d9981674f636471730f834880fca09ddc3d33ea5
Anik-36 <delighteraranik@gmail.com>
2025-11-19 00:21:09 +0600

    learn about js array merging

--- 04-Developer-skills-and-editor-setup/01-calculate-average-amplitude/script.js
+++ 04-Developer-skills-and-editor-setup/01-calculate-average-amplitude/script.js
@@ -16,3 +16,24 @@ const calcAverageAmplitude = function (temps) {
 
 const amplitude = calcAverageAmplitude(temparature);
 console.log(amplitude);
+
+console.log('------------new two array is merged-----------------');
+
+const calcAverageAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    let currTemp = temps[i];
+    if (typeof currTemp !== 'number') continue;
+
+    if (currTemp > max) max = currTemp;
+    if (currTemp < min) min = currTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeNew = calcAverageAmplitudeNew([3, 2, -1, 5], [5, 2, 9, 13]);
+console.log(amplitudeNew);
