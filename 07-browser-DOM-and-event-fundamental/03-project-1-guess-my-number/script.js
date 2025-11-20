f0614811b187f648140f2e0872161cba65aaf840
Anik-36 <delighteraranik@gmail.com>
2025-11-20 20:29:29 +0600

    win lose logic implemented

--- 07-browser-DOM-and-event-fundamental/03-project-1-guess-my-number/script.js
+++ 07-browser-DOM-and-event-fundamental/03-project-1-guess-my-number/script.js
@@ -12,11 +12,35 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (secretNumber > guess) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😔 You Lost the Game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (secretNumber < guess) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😔 You Lost the Game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
