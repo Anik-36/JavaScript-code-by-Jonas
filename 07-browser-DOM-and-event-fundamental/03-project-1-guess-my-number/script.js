b351d6245a1f3d6a5239598502e61347b826133c
Anik-36 <delighteraranik@gmail.com>
2025-11-26 15:32:48 +0600

    set the highscore of the code

--- 07-browser-DOM-and-event-fundamental/03-project-1-guess-my-number/script.js
+++ 07-browser-DOM-and-event-fundamental/03-project-1-guess-my-number/script.js
@@ -14,7 +14,7 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
+let highscore = 0;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -30,6 +30,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     //if the guess is too low
   } else if (secretNumber < guess) {
