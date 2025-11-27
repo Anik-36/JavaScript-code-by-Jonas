7e1cf1d3231ea113d15730665bbdaa0b4981feb4
Anik-36 <delighteraranik@gmail.com>
2025-11-27 22:33:20 +0600

    a very intertesing event listener project is done

--- 07-browser-DOM-and-event-fundamental/04-Modal-project/script.js
+++ 07-browser-DOM-and-event-fundamental/04-Modal-project/script.js
@@ -5,7 +5,6 @@ const btnOpenModal = document.querySelectorAll('.show-modal');
 const overlay = document.querySelector('.overlay');
 
 const openModal = function () {
-  console.log('Button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -14,6 +13,8 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
+// console.log(btnOpenModal);
+// it is basically a node list that is an array.
 for (let i = 0; i < btnOpenModal.length; i++) {
   btnOpenModal[i].addEventListener('click', openModal);
 }
@@ -21,3 +22,12 @@ for (let i = 0; i < btnOpenModal.length; i++) {
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // keyboard press is basically global eventListener. that means it works for all time not a specific time. and thats why we use document object(its basically a bunch of methods) and add direct eventlistener. and use a function parameter (e) that is all a object . and that properties is "key" what we press is stored in it and can be manipulated in this function.
+  //   console.log(e);
+  //   console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
