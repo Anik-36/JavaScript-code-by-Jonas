22bc9663ac1c9d4fdfd1387c7fe374d8a76f0362
Anik-36 <delighteraranik@gmail.com>
2025-11-27 15:30:48 +0600

    use the popup funcionality

--- 07-browser-DOM-and-event-fundamental/04-Modal-project/script.js
+++ 07-browser-DOM-and-event-fundamental/04-Modal-project/script.js
@@ -1,9 +1,23 @@
 'use strict';
-const modal = document.querySelector('modal');
+const modal = document.querySelector('.modal');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 const overlay = document.querySelector('.overlay');
 
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnOpenModal.length; i++) {
-  console.log(btnOpenModal[i].textContent);
+  btnOpenModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
