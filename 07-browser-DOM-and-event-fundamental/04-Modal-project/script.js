aa85744eeed1f7e99acade55b9ce358e352faf2b
Anik-36 <delighteraranik@gmail.com>
2025-11-27 14:19:52 +0600

    selector into variable

--- 07-browser-DOM-and-event-fundamental/04-Modal-project/script.js
+++ 07-browser-DOM-and-event-fundamental/04-Modal-project/script.js
@@ -1,1 +1,9 @@
 'use strict';
+const modal = document.querySelector('modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+const overlay = document.querySelector('.overlay');
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  console.log(btnOpenModal[i].textContent);
+}
