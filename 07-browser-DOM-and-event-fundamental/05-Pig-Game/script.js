80439d83953dfcac512b00f6cbef3345bd159630
Anik-36 <delighteraranik@gmail.com>
2025-11-30 14:34:57 +0600

    set the current score for player1

--- 07-browser-DOM-and-event-fundamental/05-Pig-Game/script.js
+++ 07-browser-DOM-and-event-fundamental/05-Pig-Game/script.js
@@ -3,10 +3,15 @@
 // selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const currentScore0El = document.getElementById('current--0');
+const currentScore1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
+let currentScore = 0;
 
 // starting condition
 score0El.textContent = 0;
@@ -22,5 +27,12 @@ btnRoll.addEventListener('click', function () {
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
 
-  // 3. check for rolled 1 : if true , switch to next player
+  // 3. check for rolled 1 : if true
+  if (dice !== 1) {
+    // 1. Add the number to current score
+    currentScore += dice;
+    currentScore0El.textContent = currentScore;
+  } else {
+    // Switch to next player
+  }
 });
