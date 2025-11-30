d98ecad6dda6bf7c284281a16ab0a277aa87cc81
Anik-36 <delighteraranik@gmail.com>
2025-11-30 17:10:09 +0600

    if one player win then the game will stop

--- 07-browser-DOM-and-event-fundamental/05-Pig-Game/script.js
+++ 07-browser-DOM-and-event-fundamental/05-Pig-Game/script.js
@@ -16,6 +16,7 @@ const btnHold = document.querySelector('.btn--hold');
 let currentScore = 0;
 let activePlayer = 0;
 const scores = [0, 0];
+let playing = true;
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
@@ -30,32 +31,48 @@ score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 btnRoll.addEventListener('click', function () {
-  // 1. Generate a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+  if (playing) {
+    // 1. Generate a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // console.log(dice);
 
-  // 2. Display the dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    // 2. Display the dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  // 3. check for rolled 1 : if true
-  if (dice !== 1) {
-    // 1. Add the number to current score
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // Switch to next player
-    switchPlayer();
+    // 3. check for rolled 1 : if true
+    if (dice !== 1) {
+      // 1. Add the number to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
   }
 });
 
 btnHold.addEventListener('click', function () {
-  // 1. Add current score to Total score
-  scores[activePlayer] += currentScore;
-  document.getElementById(`score--${activePlayer}`).textContent =
-    scores[activePlayer];
-  // 2. Check if it cross 100 or not
-  // 3. if not , switch to next player
-  switchPlayer();
+  if (playing) {
+    // 1. Add current score to Total score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // 2. Check if it cross 100 or not
+    if (scores[activePlayer] >= 20) {
+      playing = false;
+      diceEl.classList.add('hidden');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // 3. if not , switch to next player
+      switchPlayer();
+    }
+  }
 });
