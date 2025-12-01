64070b7477c1f7185a875c72a5bc3d5b0be3bc99
Anik-36 <delighteraranik@gmail.com>
2025-12-01 14:52:52 +0600

    take the starter file of javascript deep knowledge

--- 07-browser-DOM-and-event-fundamental/05-Pig-Game/script.js
+++ 07-browser-DOM-and-event-fundamental/05-Pig-Game/script.js
@@ -72,7 +72,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. Check if it cross 100 or not
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
 
