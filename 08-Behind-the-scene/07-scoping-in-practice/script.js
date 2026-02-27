3cda8fc844090e24e8f4e4cae7fe8b5be29ffb4b
Anik-36 <delighteraranik@gmail.com>
2026-02-27 20:14:39 +0600

    learning about execution code in js from procoderr

--- 08-Behind-the-scene/07-scoping-in-practice/script.js
+++ 08-Behind-the-scene/07-scoping-in-practice/script.js
@@ -1,1 +1,9 @@
 'use strict';
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  console.log(firstName);
+  return age;
+}
+
+let firstName = 'jonas';
+calcAge(1991);
---
+++ 08-Behind-the-scene/Procoderr/code_execution_in_js/script.js
@@ -0,0 +1,5 @@
+var firstName = "Akash";
+let lastName = "Singh";
+let age = 15;
+const yearOfBirth = 1996;
+let userIntro = "HI , my name is" + firstName + " " + lastName;
