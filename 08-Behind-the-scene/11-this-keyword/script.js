8eb3722d719f5ab802f8bf3e69952bb046579e88
Anik-36 <delighteraranik@gmail.com>
2026-03-07 17:43:23 +0600

    learning about this keyword

---
+++ 08-Behind-the-scene/11-this-keyword/script.js
@@ -0,0 +1,11 @@
+'use strict';
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  firstName = 'Steven';
+  console.log(firstName);
+  return age;
+}
+
+let firstName = 'jonas';
+calcAge(1991);
+// learning hoisting
