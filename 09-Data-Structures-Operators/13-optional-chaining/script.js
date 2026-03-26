cc9712d09691066f2fddcce54371716898d8b85a
Anik-36 <delighteraranik@gmail.com>
2026-03-26 20:46:54 +0600

    learning about optional chaning

---
+++ 09-Data-Structures-Operators/13-optional-chaining/script.js
@@ -0,0 +1,108 @@
+'use strict';
+
+// Data needed for a later exercise
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// const italianFoods = new Set([
+//   'pasta',
+//   'gnocchi',
+//   'tomatoes',
+//   'olive oil',
+//   'garlic',
+//   'basil',
+// ]);
+
+// const mexicanFoods = new Set([
+//   'tortillas',
+//   'beans',
+//   'rice',
+//   'tomatoes',
+//   'avocado',
+//   'garlic',
+// ]);
+
+// Data needed for first part of the section
+
+// 3rd enhancement is able to calculate in the object literal like this.
+const weekdays = ['mon', 'tues', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  // ES6 version
+  // if i need a object from outside, just write the similar name of the object.
+
+  openingHours,
+
+  // another enhancement is writing a method without typical function expression
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '22:00', address }) {
+    console.log(
+      `Order is received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time} `,
+    );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious Pasta with ${ing1}, ${ing2} and ${ing3}`,
+    );
+  },
+
+  // old way to write a function
+  orderPizza: function (mainIngredients, ...otherIngredients) {
+    console.log(mainIngredients);
+    console.log(otherIngredients);
+  },
+};
+
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining
+
+// only if the before element of question mark exits, then it goes to next , otherwise immediately return undefined
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// console.log(restaurant.openingHours.fri.open);
+
+const days = ['mon', 'tues', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'Closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// METHOD
+
+console.log(restaurant.order?.(0, 1) ?? "Method doesn't exists");
+console.log(restaurant.orderRisotto?.(0, 1) ?? "Method doesn't exists");
+
+// ARRAY
+
+const user = [{ name: 'Jonas', email: 'hello@gamil.io ' }];
+// const user = [];
+
+console.log(user[0]?.name ?? "User array doesn't exist empty ");
