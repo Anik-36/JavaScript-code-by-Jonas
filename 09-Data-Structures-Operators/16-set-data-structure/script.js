581945762e2c933d8f953e40898af1cafb381097
Anik-36 <delighteraranik@gmail.com>
2026-03-28 16:10:40 +0600

    learnt set

--- 09-Data-Structures-Operators/16-set-data-structure/script.js
+++ 09-Data-Structures-Operators/16-set-data-structure/script.js
@@ -8,3 +8,29 @@ const orderSet = new Set([
   'Pizza',
 ]);
 console.log(orderSet);
+
+console.log(new Set('Jonas'));
+
+console.log(orderSet.size);
+
+console.log(orderSet.has('Pizza'));
+console.log(orderSet.has('Bread'));
+orderSet.add('Garlic Bread');
+orderSet.add('Garlic Bread');
+orderSet.delete('Risotto');
+// orderSet.clear();
+console.log(orderSet);
+
+for (const order of orderSet) console.log(order);
+
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size,
+);
+
+console.log(new Set('anisur rahman anik').size);
