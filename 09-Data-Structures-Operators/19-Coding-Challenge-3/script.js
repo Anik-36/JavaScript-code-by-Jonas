47a0ed2f73962f24238087069d1031f7b8ad19f2
Anik-36 <delighteraranik@gmail.com>
2026-03-30 21:50:24 +0600

    challenge 3 is done

--- 09-Data-Structures-Operators/19-Coding-Challenge-3/script.js
+++ 09-Data-Structures-Operators/19-Coding-Challenge-3/script.js
@@ -34,18 +34,19 @@ const gameEvents = new Map([
   [92, '🔶 Yellow card'],
 ]);
 
-const events = new Set([...gameEvents.values()]);
+const events = [...new Set(gameEvents.values())];
 console.log(events);
 
 gameEvents.delete(64);
 console.log(gameEvents);
 
-for (const [key, value] of gameEvents) {
-  if (key <= 45) {
-    console.log(`[FIRST HALF] ${key}:
-            ${value}`);
-  } else {
-    console.log(`[SECOND HALF] ${key}:
-            ${value}`);
-  }
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`,
+);
+
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
 }
