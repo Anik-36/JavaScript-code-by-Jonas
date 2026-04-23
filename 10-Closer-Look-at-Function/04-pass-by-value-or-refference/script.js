4ffda4bc86c0823d29ce474e353efe3165876383
Anik-36 <delighteraranik@gmail.com>
2026-04-23 21:00:45 +0600

    learnt about pass by value and ref topic

--- 10-Closer-Look-at-Function/04-pass-by-value-or-refference/script.js
+++ 10-Closer-Look-at-Function/04-pass-by-value-or-refference/script.js
@@ -1,25 +1,33 @@
 'use strict';
 
-const bookings = [];
-
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers,
-) {
-  // ES5
-  //   numPassengers = numPassengers || 1;
-  //   price = price || 199;
-
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedmann',
+  passport: 2349895298,
 };
 
-createBooking('LH123');
-createBooking('LH123', 3);
-createBooking('LH123', undefined, 900);
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 2349895298) {
+    alert('Checked In.');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same doing....
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
