f16c6d19a73243c9fc37dc08eafd192a6bc93e07
Anik-36 <delighteraranik@gmail.com>
2026-04-25 19:31:55 +0600

    learnt about bind method

---
+++ 10-Closer-Look-at-Function/09-bind-method/script.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  booking: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`,
+    );
+
+    this.booking.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(36, 'Kein Williams');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  booking: [],
+};
+
+const book = lufthansa.book;
+// Doesn't work. because book is a regular function and this keyword will be undefined in a regular function.
+// book(23, 'Sarah Williams');
+
+// Call method -> takes arguments as the object and function paremeter
+book.call(eurowings, 237, 'Corry Madison');
+console.log(eurowings);
+
+book.call(lufthansa, 209, 'Marry Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Line',
+  iataCode: 'LX',
+  booking: [],
+};
+
+book.call(swiss, 792, 'Anisur Rahman Anik');
+console.log(swiss);
+
+// Apply method -> takes arguments as the object and array
+const flightData = [983, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
+console.log(swiss);
+
+// Bind method -> it returns a function for specific location . and we can also give other paremeters by default for make it simple for further easy use. EX: bookEW23
+
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+const bookEW = book.bind(eurowings);
+
+bookEW(233, 'Martha Zayrados');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Anisur Rahman Anik');
