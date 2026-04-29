8e1f957373cb58882831014709f67158511fad73
Anik-36 <delighteraranik@gmail.com>
2026-04-29 15:53:02 +0600

    learn about IIFE

---
+++ 10-Closer-Look-at-Function/12-immediately-invoked-function-expression/script.js
@@ -0,0 +1,23 @@
+const runOnce = function () {
+  console.log('This will run once');
+};
+runOnce();
+
+// IIFE = Immediately Invoked Function Expression
+
+// create a expression not statement
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})();
+
+// console.log(isPrivate);
+
+(() => console.log('This will never run Once again'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 44;
+}
+// console.log(isPrivate);
+console.log(notPrivate);
