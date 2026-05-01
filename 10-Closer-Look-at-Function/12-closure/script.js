1225a7041f2fa08ce9ccdf5af62fe1b156400c8d
Anik-36 <delighteraranik@gmail.com>
2026-05-01 12:18:59 +0600

    Changing tha name of the folders

--- 10-Closer-Look-at-Function/12-closure/script.js
+++ 10-Closer-Look-at-Function/12-closure/script.js
@@ -11,4 +11,6 @@ booker();
 booker();
 booker();
 
+console.dir(booker);
+
 // Closure is a concept that makes the connection between function and its birthplace execution context. so the function can use all the variable of its parent scope. and it is much priorities than scope chain. let say passengerCount = 10 in the global scope. but it will defined as 0 because of closure . cause it has more priority.
