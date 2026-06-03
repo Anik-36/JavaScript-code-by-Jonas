0ae74f66688dd0d3d4b24a2d45a34efdb820d865
Anik-36 <delighteraranik@gmail.com>
2026-06-03 19:38:49 +0600

    just using modern way

--- 11-Working-with-arrays/15-coding-challenge2/script.js
+++ 11-Working-with-arrays/15-coding-challenge2/script.js
@@ -121,11 +121,17 @@ GOOD LUCK 😀
 const calcAverageHumanAge = function (ages) {
   const humanAge = ages.map((age) => (age <= 2 ? 2 * age : 16 + age * 4));
   console.log(humanAge);
-  const newAge = humanAge.filter((age) => age >= 18);
-  console.log(newAge);
-  const average = newAge.reduce((acc, age) => (acc += age), 0);
-  console.log(average);
-  console.log(average / newAge.length);
+  const adults = humanAge.filter((age) => age >= 18);
+  // const average = adults.reduce((acc, age) => (acc += age), 0) / adults.length;
+  // 2 3 => (2+3)/2 = 2.5 === 2/2 + 3/2 = 2.5
+  const average = adults.reduce(
+    (acc, age, i, arr) => (acc += age / arr.length),
+    0,
+  );
+  return average;
 };
 
-calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avg1, avg2);
